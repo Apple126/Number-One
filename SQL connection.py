ed858418880b920504e57e435d790a86f6bf7a0b
Aliaksei Lubianau <aliaksei.lubianau@innowise.com>
2024-06-11 18:26:19 +0300

    result

--- SQL connection.py
+++ SQL connection.py
@@ -1,12 +1,6 @@
 import os
-
 import pandas as pd
-import sqlalchemy
 from sqlalchemy import create_engine
-from sqlalchemy import text
-
-# students_df = pd.read_json(r'C:\Users\User\Desktop\BigData\students.json')
-# rooms_df = pd.read_json(r'C:\Users\User\Desktop\BigData\rooms.json')
 
 db_params = {
     'user': 'postgres',
@@ -27,44 +21,27 @@ for i in os.listdir(json_directory):
         json_files.append(i)
 print(f'Выбранные для загрузки в базу файлы: {json_files}')
 
-need_to_upload = input(f'Начать загрузку указанных файлов в базу {db_params['database']}? +/-')
+need_to_upload = input(f'Требуется ли загрузка указанных файлов в базу {db_params['database']}? [+ или -]: ')
 if need_to_upload == '+':
     for file in json_files:
         file_path = os.path.join(json_directory, file)
         table_name = file.split('.')[0]
         df = pd.read_json(file_path)
-        df.to_sql(name=f'{table_name}', con=f'{engine}', if_exists='replace', index=False)
-        print(f'Загрузка файла {file} в таблицу {table_name}')
+        df.to_sql(name=f'{table_name}', con=engine, if_exists='replace', index=False)
+        print(f'Загрузка данных из {file} в таблицу {table_name}')
+else:
+    print('Загрузка отменена')
 
-    else:
-        print('Загрузка отменена')
-
-# students_df.to_sql(name='students', con=engine, if_exists='replace', index=False,
-#     dtype={
-#         'birthday': sqlalchemy.types.TIMESTAMP(),
-#         'id': sqlalchemy.types.INTEGER(),
-#         'name': sqlalchemy.types.VARCHAR(length=55),
-#         'room': sqlalchemy.types.INTEGER(),
-#         'sex': sqlalchemy.types.CHAR(length=1),
-#     })
-#
-# rooms_df.to_sql(name='rooms', con=engine, if_exists='replace', index=False,
-#     dtype={
-#         'id': sqlalchemy.types.INTEGER(),
-#         'name': sqlalchemy.types.VARCHAR(length=55),
-#     })
-
+# Написание SQL запроса к базе
 need_query = True
-
 while need_query:
     do_i_need_query = input('Желаете написать SQL-запрос? [+ или -]: ')
-
     if do_i_need_query == '-':
         need_query = False
         break
 
-    print('Введите SQL-запрос. Введите пустую строку для завершения ввода: ')
-
+    print('Введите SQL-запрос. Для завершения ввода - введите пустую строку : ')
+    # Ввод самого запроса (с возможностью писать в удобном формате, как в SQL, на новых строчках)
     sql_query = ''
     while True:
         line = input()
@@ -78,15 +55,14 @@ while need_query:
     output_path = fr'C:\Users\User\Desktop\BigData\Results\{file_name}.{file_format}'
 
     # Выполнение SQL-запроса и сохранение результатов в DataFrame
-    df_sql_query = pd.read_sql_query(sql_query, engine)
+    df_result = pd.read_sql_query(sql_query, engine)
 
-    # Сохранение DataFrame в папку
-
+    # Сохранение результата в папку, с выбранным форматом
     if file_format == 'json':
-        df_sql_query.to_json(output_path, orient='records')
+        df_result.to_json(output_path, orient='records')
     elif file_format == 'xml':
-        df_sql_query.to_xml(output_path)
+        df_result.to_xml(output_path)
     elif file_format == 'csv':
-        df_sql_query.to_csv(output_path, index=True)
+        df_result.to_csv(output_path, index=True)
 
     print(f'Результат запроса в файле {file_name}.{file_format} сохранен по пути: {output_path}')
